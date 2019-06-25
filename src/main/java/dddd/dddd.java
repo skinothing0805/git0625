fc4c2181a90a9df4ead3dee7f92130a74d4ff929
ski <ski_nothing0805@163.com>
2019-06-25 12:54:40 +0800

    Initial commit

---
+++ src/main/java/dddd/dddd.java
@@ -0,0 +1,17 @@
+package dddd;
+
+import sun.applet.Main;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * @author Ski
+ * @create 2019-06-25 12:48
+ * @Dsecription
+ */
+public class dddd {
+
+    public static void main(String[] args) {
+        System.out.println("ddddddddddd");
+    }
+}
